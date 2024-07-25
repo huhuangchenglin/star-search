3b3302b692a2f4f97f33b5f63eeb756b53503592
胡黄成霖 <79182207+huhuangchenglin@users.noreply.github.com>
2024-07-25 17:21:05 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,229 @@
+<?php
+ require "common.php";
+if ($set['autokeywords'] == 1) {
+	$word = get_word();
+} else {
+	$word = "";//禁止搜索词汇
+}
+ 
+?> 
+<!DOCTYPE html>
+<html lang="zh-CN">
+ <head> 
+  <meta charset="UTF-8" /> 
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" /> 
+  <meta http-equiv="Cache-Control" content="no-siteapp" /> 
+  <meta name="apple-mobile-web-app-capable" content="yes" /> 
+  <meta name="apple-touch-fullscreen" content="yes" /> 
+  <meta name="apple-mobile-web-app-status-bar-style" content="black" /> 
+  <meta name="msvalidate.01" content="E6A585911EAC904DB98EFD927873E636" />
+  <meta name="full-screen" content="yes" />
+  <!--UC强制全屏--> 
+  <meta name="browsermode" content="application" />
+  <!--UC应用模式--> 
+  <meta name="x5-fullscreen" content="true" />
+  <!--QQ强制全屏--> 
+  <meta name="x5-page-mode" content="app" />
+  <!--QQ应用模式--> 
+  <title>星芒起始页 - Star.Search</title> 
+  <meta name="keywords" content="星芒导航,星芒搜索,星芒搜索导航,星芒,星芒起始页,起始页,搜索导航,首页,浏览器首页,胡黄成霖,百度,谷歌,谷歌搜索,必应,Star-Search,快捷导航,浏览器,星芒浏览器">
+  <meta name="description" content="最简洁的搜索导航，给你简单舒爽的搜索体验。">
+  <link rel="shortcut icon" href="img/logo.png" type="image/x-icon" /> 
+  <link href="css/style.css" rel="stylesheet" /> 
+  <link href="css/sousuo.css" rel="stylesheet" /> 
+  <link rel="stylesheet" href="css/font_1230786_gdvd1b4wlz.css" /> 
+  <link rel="stylesheet" href="css/font_1572019_yrk10qvz.css" /> 
+  <script src="js/jquery.min.js"></script> 
+  <script src="js/xd.js"></script> 
+  <script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0515664820cfc2656ae448f4fefed829";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<!--
+<style type="text/css">
+    html {
+    FILTER: gray;
+    -webkit-filter: grayscale(100%);
+    }
+</style>
+   -->
+  <!-- 黑白 -->
+ </head> 
+ <body> 
+  <div id="menu">
+   <i></i>
+  </div> 
+  <div class="list closed"> 
+   <ul>
+    <li class="title"><i class="iconfont icon-changyong"></i> 常用</li> 
+        <li><a rel="nofollow" href="https://www.taobao.com" target="_blank"><i class="iconfont icon-taobao" style="color: #ff6019;"></i>淘宝网</a></li>
+	    <li><a rel="nofollow" href="https://www.jd.com" target="_blank"><i class="iconfont icon-jingdong" style="color: #E31D1A;"></i>京东</a></li>
+	    <li><a rel="nofollow" href="https://www.weibo.com" target="_blank"><i class="iconfont icon-weibo" style="color: #e6162d;"></i>微博</a></li>
+	    <li><a rel="nofollow" href="https://www.bilibili.com/" target="_blank"><i class="iconfont icon-bili" style="color:#09e;"></i>哔哩哔哩</a></li> 
+	    <li><a rel="nofollow" href="https://fanyi.youdao.com/" target="_blank"><i class="iconfont icon-fanyi"></i>有道翻译</a></li>
+	    <li><a rel="nofollow" href="https://bigjpg.com/zh" target="_blank"><i class="iconfont icon-tupian" style="color: #00bcff;"></i>JPG放大</a></li>
+	    <li><a rel="nofollow" href="https://www.picdiet.com/zh-cn" target="_blank"><i class="iconfont icon-tupian"style="color:#fcb600"></i>JPG压缩</a></li>
+	    <li><a rel="nofollow" href="https://pan.baidu.com" target="_blank"><i class="iconfont icon-baidu" style="color: #1371ff;"></i>百度网盘</a></li>
+	    <li><a rel="nofollow" href="https://www.aliyundrive.com/sign/in" target="_blank"><i class="iconfont icon-cloud-download" style="color: #4bbdff;"></i>阿里云盘</a></li>
+    <li class="title"><i class="iconfont icon-youxiang"></i> 邮箱</li> 
+        <li><a rel="nofollow" href="https://mail.qq.com/" target="_blank"><i class="iconfont icon-QQ" style="color: #f05;"></i>QQ邮箱</a></li>
+        <li><a rel="nofollow" href="https://mail.163.com/" target="_blank"><i class="iconfont icon-wangyi" style="color: #169;"></i>网易邮箱</a></li> 
+    <li class="title"><i class="iconfont icon-msg"></i> 论坛</li> 
+       <li><a rel="nofollow" href="https://blog.star-search.cn/" target="_blank"><i class="iconfont icon-gmail" style="color: #f03;"></i>星芒博客</a></li>
+        <li><a rel="nofollow" href="https://www.weibo.com" target="_blank"><i class="iconfont icon-weibo" style="color: #e6162d;"></i>微博</a></li> 
+        <li><a rel="nofollow" href="https://www.zhihu.com/" target="_blank"><i class="iconfont icon-zhihu" style="color: #0084ff;"></i>知乎</a></li>
+    <li class="title"><i class="iconfont icon-shipin"></i> 媒体</li> 
+        <li><a rel="nofollow" href="https://www.bilibili.com/" target="_blank"><i class="iconfont icon-bili" style="color:#09e;"></i>哔哩哔哩</a></li> 
+        <li><a rel="nofollow" href="https://www.youtube.com/" target="_blank"><i class="iconfont icon-youtube" style="color:#f03;"></i>Youtube</a></li>
+        <li><a rel="nofollow" href="https://www.douyin.com/" target="_blank"><i class="iconfont icon-douyin" style="color:#f03;"></i>抖音</a></li>
+        <li><a rel="nofollow" href="https://www.yhdmz.org/" target="_blank"><i class="iconfont icon-acfun" style="color:#f33;"></i>樱花动漫</a></li> 
+        <li><a rel="nofollow" href="https://music.163.com/" target="_blank"><i class="iconfont icon-wangyi" style="color:#f03;"></i>网易云音乐</a></li> 
+    <li class="title"><i class="iconfont icon-ai-tool"></i> 工具</li> 
+        <li><a rel="nofollow" href="https://pan.star-search.cn/" target="_blank"><i class="iconfont icon-cloud-download" style="color: #ff6019;"></i>星芒下载站</a></li>
+        <li><a rel="nofollow" href="https://tool.star-search.cn/" target="_blank"><i class="iconfont icon-wangluo1" style="color: #ff6019;"></i>星芒工具箱</a></li>
+        <li><a rel="nofollow" href="https://yiyan.baidu.com/" target="_blank"><i class="iconfont icon-baidu" style="color: #ff6019;"></i>文心一言</a></li> 
+        <li><a rel="nofollow" href="https://pan.baidu.com" target="_blank"><i class="iconfont icon-baiduyun" style="color: #148bfe;"></i>百度网盘</a></li>
+        <li><a rel="nofollow" href="https://www.aliyundrive.com/sign/in" target="_blank"><i class="iconfont icon-cloud-download" style="color: #4bbdff;"></i>阿里云盘</a></li>
+        <li><a rel="nofollow" href="https://vocalremover.org/ch/" target="_blank"><i class="iconfont icon-video" style="color: #4bbdff;"></i>音频编辑</a></li> 
+    <li class="title"><i class="iconfont icon-kongzhitai"></i> 开发</li> 
+        <li><a rel="nofollow" href="https://www.githubs.cn/" target="_blank"><i class="iconfont icon-github"></i>Github</a></li> 
+        <li><a rel="nofollow" href="https://msdn.itellyou.cn/" target="_blank"><i class="iconfont icon-windows" style="color: #48c;"></i>MSDN</a></li> 
+        <li><a rel="nofollow" href="https://ping.pe" target="_blank"><i class="iconfont icon-wangluo1" style="color:#1d0;"></i>Ping.pe</a></li> 
+        <li><a rel="nofollow" href="https://ping.chinaz.com/" target="_blank"><i class="iconfont icon-pingup" style="color:#2361ad;"></i>站长Ping</a></li>
+    <li class="title">AI</li> 
+        <li><a rel="nofollow" href="https://yiyan.baidu.com/" target="_blank"><i class="" style="color: #4bbdff;"></i>文心一言</a></li> 
+        <li><a rel="nofollow" href="https://tongyi.aliyun.com/" target="_blank"><i class="" style="color: #4bbdff;"></i>通义千问</a></li> 
+        <li><a rel="nofollow" href="https://chat.openai.com/" target="_blank"><i class="" style="color: #4bbdff;"></i>ChatGPT</a></li> 
+        <li><a rel="nofollow" href="https://xinghuo.xfyun.cn/" target="_blank"><i class="" style="color: #4bbdff;"></i>星火大模型</a></li>
+    <li class="title"></i>关于我们</li> 
+         <li><a rel="nofollow" href="https://about.star-search.cn/" target="_blank"><i class="" style="color: #4bbdff;"></i>关于我</a></li> 
+         <li><a rel="nofollow" href="https://blog.star-search.cn/?post=24" target="_blank"><i class="" style="color: #4bbdff;"></i>捐助我</a></li> 
+         <li><a rel="nofollow" href="https://blog.star-search.cn/?post=29" target="_blank"><i class="" style="color: #4bbdff;"></i>下载客户端</a></li>
+         <li><a rel="nofollow" href="https://blog.star-search.cn/?post=77" target="_blank"><i class="" style="color: #4bbdff;"></i>漏洞意见反馈</a></li>
+         <li><a rel="nofollow" href="https://blog.star-search.cn/?post=78" target="_blank"><i class="" style="color: #4bbdff;"></i>更新日志</a></li>
+         <li><a rel="nofollow" href="https://blog.star-search.cn/?post=62" target="_blank"><i class="" style="color: #4bbdff;"></i>背景图片下载</a></li>
+<!--         <li><a rel="nofollow" href="https://upload.star-search.cn/" target="_blank"><i class="iconfont icon-cloud-download-alt" style="color: #4bbdff;"></i>文件上传</a></li>   -->
+         <li><a rel="nofollow" href="https://jq.qq.com/?_wv=1027&k=A6wxje1W" target="_blank"><i class="iconfont icon-cloud-download-alt" style="color: #4bbdff;"></i>官方QQ群</a></li>
+    <li class="title"></i>当前版本号：20240403</li> 
+<!--    <li class="title"></i>友情链接</li>
+        <li><a rel="nofollow" href="https://start.geeklab.work" target="_blank"><i class="iconfont icon-cloud-download-alt" style="color: #4bbdff;"></i>轻云起始页</a></li>     -->
+   </ul> 
+  </div> 
+  <div id="search" class="s-search">
+   <div id="search-list" class="hide-type-list">
+    <div class="s-type">
+     <span></span>
+     <div class="s-type-list animated fadeInUp">
+      <label for="type-search">搜索</label>
+      <label for="type-br">工具</label>
+      <label for="type-zhihu">社区</label>
+     </div>
+    </div>
+    <div class="search-group group-a">
+     <span class="type-text">搜索</span>
+     <ul class="search-type">
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-search" value="https://www.baidu.com/s?wd=" data-placeholder="百度一下" /><label for="type-search"><span>百度</span></label></li>
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-google" value="https://www.google.com/search?q=" data-placeholder="谷歌搜索" /><label for="type-google"><span>Google</span></label></li>
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-bing" value="https://cn.bing.com/search?q=" data-placeholder="微软Bing搜索" /><label for="type-bing"><span>Bing</span></label></li>
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-web" value="" data-placeholder="请输入网址" /><label for="type-web"><span>网址访问</span></label></li>
+     </ul>
+    </div>
+    <div class="search-group group-b">
+     <span class="type-text">工具</span>
+     <ul class="search-type">
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-br" value="http://rank.chinaz.com/all/" data-placeholder="请输入网址(不带http://)" /><label for="type-br"><span>权重查询</span></label></li>
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-links" value="http://link.chinaz.com/" data-placeholder="请输入网址(不带http://)" /><label for="type-links"><span>友链检测</span></label></li>
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-ping" value="http://ping.chinaz.com/" data-placeholder="请输入网址(不带http://)" /><label for="type-ping"><span>PING检测</span></label></li>
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-404" value="http://tool.chinaz.com/Links/?DAddress=" data-placeholder="请输入网址(不带http://)" /><label for="type-404"><span>死链检测</span></label></li>
+
+     </ul>
+    </div>
+    <div class="search-group group-c">
+     <span class="type-text">社区</span>
+     <ul class="search-type">
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-zhihu" value="https://www.zhihu.com/search?type=content&amp;q=" data-placeholder="知乎" /><label for="type-zhihu"><span>知乎</span></label></li>
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-weibo" value="http://s.weibo.com/weibo/" data-placeholder="微博" /><label for="type-weibo"><span>微博</span></label></li>
+      <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-快递100" value="http://www.kuaidi100.com/?" data-placeholder="快递100" /><label for="type-快递100"><span>快递100</span></label></li>
+     <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-github" value="https://github.com/search?q=" data-placeholder="请输入想在Github搜索的项目" /><label for="type-github"><span>Github</span></label></li>
+    <li><input onclick="bw()" hidden="" type="radio" name="type" id="type-bili" value="https://search.bilibili.com/all?keyword=" data-placeholder="请输入在Bilibili上想搜索关键词" /><label for="type-bili"><span>哔哩哔哩</span></label></li>
+     </ul>
+    </div>
+   </div>
+   <form action="https://www.baidu.com/s?wd=" method="get" target="_blank" id="super-search-fm">
+    <input type="text" id="search-text" placeholder="输入关键字搜索" style="outline:0" />
+    <button type="submit"><i class="iconfont icon-xdss"></i></button>
+    <ul id="ul" class="ko"></ul>
+   </form>
+   <div id="yy" style="display:none;">
+    <input type="text" id="kos" placeholder="<?php echo $word; ?>" />
+    <button type="submit" id="kob"><i class="iconfont icon-xdss"></i></button>
+    <ul id="kol" class="ko"></ul>
+   </div>
+   <div class="set-check hidden-xs">
+    <input type="checkbox" id="set-search-blank" class="bubble-3" autocomplete="off" />
+   </div>
+  </div> 
+  <script type="text/javascript" src="js/yyss.js"></script>
+  <script type="text/javascript" src="js/sousuo.js"></script> 
+  <script type="text/javascript" src="js/lianxiang.js"></script> 
+  <div class="bgo"></div> 
+ </body>
+ <!--天气插件开始-->
+			<!-- CD_weather -->
+
+			<!--天气插件结束-->
+<div style="position: fixed;width: 100%;height: 30px;line-height: 30px;bottom: 0;left: 0;text-align: center;">
+			<a style="color: #fff;" href="https://beian.miit.gov.cn/" target="_blank">蜀ICP备2021021854号-2</a>
+			<a  style="color: #fff;margin-left: 20px;cursor: pointer;" onclick="jiu()">切换旧版</a>
+			</div>
+</html>
+
+<script>
+	function jiu(){
+		setCookie('style',1,30);
+		window.location.href="old/index.html"
+	}
+	
+	function setCookie(name, value, liveMinutes) {
+		if (liveMinutes == undefined || liveMinutes == null) {
+			liveMinutes = 60 * 2;
+		}
+		if (typeof (liveMinutes) != 'number') {
+			liveMinutes = 60 * 2;//默认120分钟
+		}
+		var minutes = liveMinutes * 60 * 1000;
+		var exp = new Date();
+		exp.setTime(exp.getTime() + minutes + 8 * 3600 * 1000);
+		//path=/表示全站有效，而不是当前页
+		document.cookie = name + "=" + value + ";path=/;expires=" + exp.toUTCString();
+	}
+
+	
+</script>
+
+
+<script>
+	function getCookie(cname)
+	{
+	  var name = cname + "=";
+	  var ca = document.cookie.split(';');
+	  for(var i=0; i<ca.length; i++) 
+	  {
+	    var c = ca[i].trim();
+	    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+	  }
+	  return "";
+	}
+	var style=getCookie('style');
+	console.log(style)
+	if(style==1){
+		window.location.href="old/"
+	}
+	
+	
+</script>
